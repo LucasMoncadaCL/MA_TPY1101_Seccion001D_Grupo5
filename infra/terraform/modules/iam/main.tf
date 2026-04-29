b0db4e4e61fec0b7103b2d0066964004146c6898
Cesitar16 <cesarrojasramos160@gmail.com>
2026-04-29 04:44:49 -0400

    fix(infra): grant actAs permission for Cloud Run runtime service account

--- infra/terraform/modules/iam/main.tf
+++ infra/terraform/modules/iam/main.tf
@@ -10,3 +10,10 @@ resource "google_project_iam_member" "runtime_roles" {
   role     = each.value
   member   = "serviceAccount:${google_service_account.runtime.email}"
 }
+
+resource "google_service_account_iam_member" "runtime_sa_user" {
+  for_each           = toset(var.service_account_user_members)
+  service_account_id = google_service_account.runtime.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = each.value
+}
